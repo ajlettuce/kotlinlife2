6e1cd15c4d16d0e8ab7fd53a09ff058856e2a2be
Alex Lettuce <ajlettuce1@gmail.com>
2023-03-18 21:51:20 -0700

    t

--- src/main/kotlin/Life.kt
+++ src/main/kotlin/Life.kt
@@ -11,7 +11,6 @@ class Life(var alive: MutableList<Pos>) {
     fun getPopulation(): Int{
         return alive.size
     }
-    //test
     private fun addToNeighbors(id : Pos, list : MutableMap<Pos,Int>) { //slow for some reason
         for(i in nums) {
             val newPos = id + i
