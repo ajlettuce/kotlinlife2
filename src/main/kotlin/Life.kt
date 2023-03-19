465fcb0bf30dde3cee6224762bc28575889158a3
Alex Lettuce <ajlettuce1@gmail.com>
2023-03-18 21:55:29 -0700

    t

--- src/main/kotlin/Life.kt
+++ src/main/kotlin/Life.kt
@@ -1,35 +1,36 @@
 class Life(var alive: MutableList<Pos>) {
-    private var state = mutableMapOf<Pos,Int>()
-    private var fstate = mutableMapOf<Pos,Int>()
-    private var falive = mutableListOf<Pos> ()
+    private var state = mutableMapOf<Pos, Int>()
+    private var fstate = mutableMapOf<Pos, Int>()
+    private var falive = mutableListOf<Pos>()
+
     init {
-        for(i in alive)
-            add2Neighbors(i,state)
+        for (i in alive)
+            add2Neighbors(i, state)
     }
 
-    fun getPopulation(): Int{
+    fun getPopulation(): Int {
         return alive.size
     }
 
-    private fun add2Neighbors(id : Pos, list : MutableMap<Pos, Int>) { //way more efficient lol
-        list[id + Pos(-1,-1)] = list[id + Pos(-1,-1)]?.plus(1) ?: 1
-        list[id + Pos(-1,0)] = list[id + Pos(-1,0)]?.plus(1) ?: 1
-        list[id + Pos(-1,1)] = list[id + Pos(-1,1)]?.plus(1) ?: 1
-        list[id + Pos(0,-1)] = list[id + Pos(0,-1)]?.plus(1) ?: 1
-        list[id + Pos(0,1)] = list[id + Pos(0,1)]?.plus(1) ?: 1
-        list[id + Pos(1,-1)] = list[id + Pos(1,-1)]?.plus(1) ?: 1
-        list[id + Pos(1,0)] = list[id + Pos(1,0)]?.plus(1) ?: 1
-        list[id + Pos(1,1)] = list[id + Pos(1,1)]?.plus(1) ?: 1
+    private fun add2Neighbors(id: Pos, list: MutableMap<Pos, Int>) { //way more efficient lol
+        list[id + Pos(-1, -1)] = list[id + Pos(-1, -1)]?.plus(1) ?: 1
+        list[id + Pos(-1, 0)] = list[id + Pos(-1, 0)]?.plus(1) ?: 1
+        list[id + Pos(-1, 1)] = list[id + Pos(-1, 1)]?.plus(1) ?: 1
+        list[id + Pos(0, -1)] = list[id + Pos(0, -1)]?.plus(1) ?: 1
+        list[id + Pos(0, 1)] = list[id + Pos(0, 1)]?.plus(1) ?: 1
+        list[id + Pos(1, -1)] = list[id + Pos(1, -1)]?.plus(1) ?: 1
+        list[id + Pos(1, 0)] = list[id + Pos(1, 0)]?.plus(1) ?: 1
+        list[id + Pos(1, 1)] = list[id + Pos(1, 1)]?.plus(1) ?: 1
     }
 
     fun nextGen() {
-        for((k,v) in state) {
-            if(v == 3){
+        for ((k, v) in state) {
+            if (v == 3) {
                 falive.add(k)
-                add2Neighbors(k,fstate)
-            } else if(alive.contains(k) && v == 2){
+                add2Neighbors(k, fstate)
+            } else if (alive.contains(k) && v == 2) {
                 falive.add(k)
-                add2Neighbors(k,fstate)
+                add2Neighbors(k, fstate)
             }
         }
         //tested, this is the most efficient:
--- src/main/kotlin/Pos.kt
+++ src/main/kotlin/Pos.kt
@@ -1,10 +1,12 @@
 @JvmInline
 value class Pos(val pos: Long) {
-    constructor(x: Int,y: Int) : this((x.toLong() shl 32) or (y.toLong() and 0xffffffffL))
+    constructor(x: Int, y: Int) : this((x.toLong() shl 32) or (y.toLong() and 0xffffffffL))
+
     private val x: Int
         get() = (pos shr 32).toInt()
     private val y: Int
         get() = pos.toInt()
-    operator fun plus(other: Pos) = Pos(this.x + other.x,this.y + other.y)
+
+    operator fun plus(other: Pos) = Pos(this.x + other.x, this.y + other.y)
     override fun toString() = "[${this.x},${this.y}]"
 }
